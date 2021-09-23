e34a12b72f3213a997d4d910283d502d63bd89b0
Lorenzo <lorenzocardoso@hotmail.com>
2021-09-23 08:10:19 -0300

    Encaminhando token csrf para o componente Login

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -3,10 +3,11 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Login (Carregando componente Vue)</div>
+                    <div class="card-header">Login</div>
 
                     <div class="card-body">
                         <form method="POST" action="">
+                            <input type="hidden" name='_token' :value="csrf_token">
                             <div class="form-group row">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
 
@@ -55,5 +56,7 @@
 </template>
 
 <script>
-   
+   export default{
+       props:['csrf_token'] //data (semelhante)
+   }
 </script>
