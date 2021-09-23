b564bde7318cc7003040059ab9a7962808d02d11
Lorenzo <lorenzocardoso@hotmail.com>
2021-09-23 10:38:13 -0300

    Implementando autenticação WEB (session) e API (jwt)

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -4,15 +4,14 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Login</div>
-
                     <div class="card-body">
-                        <form method="POST" action="">
+                        <form method="POST" action="" @submit.prevent="login($event)">
                             <input type="hidden" name='_token' :value="csrf_token">
                             <div class="form-group row">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus>
+                                    <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus v-model="email">
                                 </div>
                             </div>
 
@@ -20,7 +19,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password" v-model="password">
                                 </div>
                             </div>
 
@@ -57,6 +56,34 @@
 
 <script>
    export default{
-       props:['csrf_token'] //data (semelhante)
+       props:['csrf_token'], //data (semelhante)
+       data() {
+           return {
+               email: '',
+               password: ''
+           }
+       } ,
+       methods: {
+           login(e){
+               
+               let url = 'http://localhost:8000/api/login';
+               let configuracao = {
+                   method: 'post',
+                   body: new URLSearchParams({
+                       'email' : this.email,
+                       'password' : this.password
+                   })
+               }
+               fetch(url, configuracao)
+                .then(response => response.json())
+                .then(data => {
+                    if(data.token){
+                        document.cookie = 'token='+data.token+';SameSite=Lax'
+                    }
+                    //disparar evento submit padrao
+                    e.target.submit()
+                }) 
+           }
+       }
    }
 </script>
