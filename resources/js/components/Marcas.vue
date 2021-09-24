18b46fe7f6531ec316217360e85095b8909f8cfe
Lorenzo <lorenzocardoso@hotmail.com>
2021-09-24 09:16:21 -0300

    Carregando registros de marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -77,16 +77,30 @@
                    return token;
                }
            },
+       mounted(){
+           this.carregarLista()
+       },    
        data(){
            return{
                urlBase: 'http://localhost:8000/api/v1/marca',
                nomeMarca: '',
                arquivoImagem: [],
                transacaoStatus: '',
-               transacaoDetalhes: {}
+               transacaoDetalhes: {},
+               marcas: []
            }
        },
        methods:{
+           carregarLista(){
+               axios.get(this.urlBase)
+                .then(response => {
+                    this.marcas = response.data
+                    console.log(this.marcas);
+                })
+                .catch(errors => {
+                    console.log(errors)
+                })
+           },
            carregarImagem(e){
                this.arquivoImagem = e.target.files
            },
