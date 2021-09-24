8179447e25b29be2af7b91e7d2faaedf28c3a838
Lorenzo <lorenzocardoso@hotmail.com>
2021-09-24 09:21:37 -0300

    Adicionando os cabeçalhos na requisição de marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -92,7 +92,15 @@
        },
        methods:{
            carregarLista(){
-               axios.get(this.urlBase)
+
+               let config = {
+                   headers: {
+                       'Accept' : 'application/json',
+                       'Authorization' : this.token
+                   }
+                }
+
+               axios.get(this.urlBase, config)
                 .then(response => {
                     this.marcas = response.data
                     console.log(this.marcas);
